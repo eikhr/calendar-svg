c302409196c3f000d9744da3775885f7efc62419
Eik Hvattum Røgeberg <eik.rogeberg@gmail.com>
2022-07-09 13:56:33 +0200

    update calendar svg image

--- http.js
+++ http.js
@@ -10,46 +10,46 @@ export default {
 
     const date = new Date();
     return new Response(`
-      <svg xmlns="http://www.w3.org/2000/svg"
-      aria-label="Calendar" role="img"
-      viewBox="0 0 512 512">
-        <path d="M512 455c0 32-25 57-57 57H57c-32 0-57-25-57-57V128c0-31 25-57 57-57h398c32 0 57 26 57 57z" fill="#e0e7ec"/>
-        <path d="M484 0h-47c2 4 4 9 4 14a28 28 0 1 1-53-14H124c3 4 4 9 4 14A28 28 0 1 1 75 0H28C13 0 0 13 0 28v157h512V28c0-15-13-28-28-28z" fill="#dd2f45"/>
-        <g fill="#f3aab9">
-          <circle cx="470" cy="142" r="14"/>
-          <circle cx="470" cy="100" r="14"/>
-          <circle cx="427" cy="142" r="14"/>
-          <circle cx="427" cy="100" r="14"/>
-          <circle cx="384" cy="142" r="14"/>
-          <circle cx="384" cy="100" r="14"/>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48">
+        <path fill="#CFD8DC" d="M5,38V14h38v24c0,2.2-1.8,4-4,4H9C6.8,42,5,40.2,5,38z"/>
+        <path fill="#F44336" d="M43,10v6H5v-6c0-2.2,1.8-4,4-4h30C41.2,6,43,7.8,43,10z"/>
+        <g fill="#B71C1C">
+          <circle cx="36" cy="10" r="3"/>
+          <circle cx="12" cy="10" r="3"/>
+        </g>
+        <g fill="#B0BEC5">
+          <path transform="translate(3,0)" d="M33,3c-1.1,0-2,0.9-2,2v5c0,1.1,0.9,2,2,2s2-0.9,2-2V5C35,3.9,34.1,3,33,3z"/>
+          <path transform="translate(-3,0)" d="M15,3c-1.1,0-2,0.9-2,2v5c0,1.1,0.9,2,2,2s2-0.9,2-2V5C17,3.9,16.1,3,15,3z"/>
         </g>
         <text
           id="month"
-          x="32" 
-          y="164" 
+          x="24" 
+          y="13" 
           fill="#fff" 
           font-family="monospace"
-          font-size="140px"
-          style="text-anchor: left"
+          font-size="8px"
+          style="text-anchor: middle"
         >
           ${date.toLocaleString(locale, { month: "short" })}
         </text>
-        <text id="day"
-          x="256" 
-          y="400" 
+        <text 
+          id="day"
+          x="24" 
+          y="32" 
           fill="#66757f" 
           font-family="monospace"
-          font-size="256px"
+          font-size="22px"
           style="text-anchor: middle"
         >
           ${date.toLocaleString(locale, { day: "2-digit" }).replace(".", "")}
         </text>
-        <text id="weekday"
-          x="256" 
-          y="480" 
+        <text 
+          id="weekday"
+          x="24" 
+          y="40" 
           fill="#66757f" 
           font-family="monospace"
-          font-size="64px"
+          font-size="6px"
           style="text-anchor: middle"
         >
           ${date.toLocaleString(locale, { weekday: 'long' })}
